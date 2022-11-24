438ede498d00a8b15a1128a5362bdf85bd92b862
Chivu Marius <chivumarius@yahoo.com>
2022-11-24 18:24:16 +0200

    Setting Up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
@@ -5,13 +7,32 @@ import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
 // ♦♦ The "main()" Function:
-void main() {
+void main() async {
+  // ♦ Ensure Initialization of Flutter Widgets:
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    // ♦ Initialize the "Web Application":
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAt7oFJLa3gDUrJYTTE4JOmNUDx3Yd6YcU",
+        appId: "1:1080167123309:web:0261ac2542c3fa7a9b3264",
+        messagingSenderId: "1080167123309",
+        projectId: "instagram-clone-9d2a8",
+        storageBucket: "instagram-clone-9d2a8.apps pot.com",
+      ),
+    );
+  } else {
+    // ♦ Firebase Application Initialization:
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
 // ♦♦ The "MyApp" Class:
 class MyApp extends StatelessWidget {
-  // ♦♦ The Class "Cnstructor:"
+  // ♦♦ The Class "Constructor:"
   const MyApp({super.key});
 
   // ♦♦ This "build" Method of the Class:
