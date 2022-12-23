ee3ad6569a574fff18cf5c8a16dd1bd898bbe1d7
Chivu Marius <chivumarius@yahoo.com>
2022-12-23 17:11:47 +0200

    Search  Screen →  'Searching Users'  Functionality

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,11 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:instagram_clone_flutter/screens/profile_screen.dart';
-import 'package:instagram_clone_flutter/utils/colors.dart';
-import 'package:instagram_clone_flutter/utils/global_variable.dart';
+import 'package:instagram_flutter/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
+  // ♦ Constructor:
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
@@ -13,12 +11,24 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  // ♦ Controller:
   final TextEditingController searchController = TextEditingController();
+
+  // ♦ Variable:
   bool isShowUsers = false;
 
+  // ♦ The "dispose()" Method:
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
+  // ♦ The "build()" Method:
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // ♦ APP BAR:
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         title: Form(
@@ -30,13 +40,16 @@ class _SearchScreenState extends State<SearchScreen> {
               setState(() {
                 isShowUsers = true;
               });
-              print(_);
+              debugPrint(_);
             },
           ),
         ),
       ),
+
+      // ♦ BODY:
       body: isShowUsers
           ? FutureBuilder(
+              // ♦ Detting "Users" Collection:
               future: FirebaseFirestore.instance
                   .collection('users')
                   .where(
@@ -45,70 +58,39 @@ class _SearchScreenState extends State<SearchScreen> {
                   )
                   .get(),
               builder: (context, snapshot) {
+
+                // ♦ Checking:
                 if (!snapshot.hasData) {
+                  // ♦ Displaying "Loading Indicator":
                   return const Center(
                     child: CircularProgressIndicator(),
                   );
                 }
+
+                // ♦ List:
                 return ListView.builder(
+                  // ♦ Getting the "Items Number":
                   itemCount: (snapshot.data! as dynamic).docs.length,
+
                   itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => ProfileScreen(
-                            uid: (snapshot.data! as dynamic).docs[index]['uid'],
-                          ),
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          // ♦ Getting the "photoUrl" from "Database":
+                          (snapshot.data! as dynamic).docs[index]['photoUrl'],
                         ),
+                        radius: 16,
                       ),
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            (snapshot.data! as dynamic).docs[index]['photoUrl'],
-                          ),
-                          radius: 16,
-                        ),
-                        title: Text(
-                          (snapshot.data! as dynamic).docs[index]['username'],
-                        ),
+                      title: Text(
+                        // ♦ Getting the "Username" from "Database":
+                        (snapshot.data! as dynamic).docs[index]['username'],
                       ),
                     );
                   },
                 );
               },
             )
-          : FutureBuilder(
-              future: FirebaseFirestore.instance
-                  .collection('posts')
-                  .orderBy('datePublished')
-                  .get(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-
-                return StaggeredGridView.countBuilder(
-                  crossAxisCount: 3,
-                  itemCount: (snapshot.data! as dynamic).docs.length,
-                  itemBuilder: (context, index) => Image.network(
-                    (snapshot.data! as dynamic).docs[index]['postUrl'],
-                    fit: BoxFit.cover,
-                  ),
-                  staggeredTileBuilder: (index) => MediaQuery.of(context)
-                              .size
-                              .width >
-                          webScreenSize
-                      ? StaggeredTile.count(
-                          (index % 7 == 0) ? 1 : 1, (index % 7 == 0) ? 1 : 1)
-                      : StaggeredTile.count(
-                          (index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1),
-                  mainAxisSpacing: 8.0,
-                  crossAxisSpacing: 8.0,
-                );
-              },
-            ),
+          : const Text('Posts'),
     );
   }
 }
--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post_screen.dart';
 import 'package:instagram_flutter/screens/feed_screen.dart';
+import 'package:instagram_flutter/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 // ♦ The "List" of "Screens":
 List<Widget> homeScreenItems = const [
   FeedScreen(),
-  Text('Search Screen'),
+  SearchScreen(),
   AddPostScreen(),
   Text('Notifications'),
   Text('Profile Screen'),
